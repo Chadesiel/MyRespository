8fd9363b1fa80d96cc76e604c5ccda60d6d83f40
lucifer <1663408479@qq.com>
2018-12-18 17:19:43 +0800

    将Hello.java文件修改了，增加了一个循环，打印十遍

--- cn/ityuan/demo/Hello.java
+++ cn/ityuan/demo/Hello.java
@@ -2,6 +2,6 @@ package cn.ityuan.demo;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello java,I love you");
+        System.out.println("Hello java,I love you,add something ?");
     }
 }
